a8276dbe8114d594f2257d09aeb0cb91ed161832
Jeremy-96 <jeremythonon96@hotmail.com>
2025-06-04 17:34:45 +0200

    feat(structure): init ts config and add main layouts components

---
+++ src/layouts/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+export const Footer = () => {
+    return (
+        <>
+        <footer>
+            FOOTER
+        </footer>
+        </>
+    )
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import React, { StrictMode } from 'react'
-import { Container, createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.js'
+import { StrictMode } from 'react'
+import { type Container, createRoot } from 'react-dom/client'
+import { App } from './App'
+import './styles/main.css'
 
 createRoot(document.getElementById('root') as Container).render(
   <StrictMode>
